3f62bc05d3481c2962027de0d2b68601004e98f2
cnishino <96111395+cnishino@users.noreply.github.com>
2022-03-16 18:04:50 +0900

    Update 20220316081424_create_oders.rb

--- db/migrate/20220316081424_create_oders.rb
+++ db/migrate/20220316081424_create_oders.rb
@@ -8,7 +8,7 @@ class CreateOders < ActiveRecord::Migration[6.1]
       t.integer :shipping_cost, null:false
       t.integer :total_payment, null:false
       t.integer :payment_method, null:false, default: "0"
-      t.integer :status, null:false, default: ""
+      t.integer :status, null:false, default: "0"
       t.timestamps
     end
   end
