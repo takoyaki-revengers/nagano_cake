e634d8439ae7b4e18c747314c8c385d097ce8c1d
cnishino <96111395+cnishino@users.noreply.github.com>
2022-03-16 18:02:01 +0900

    Update 20220316081424_create_oders.rb

--- db/migrate/20220316081424_create_oders.rb
+++ db/migrate/20220316081424_create_oders.rb
@@ -1,6 +1,7 @@
 class CreateOders < ActiveRecord::Migration[6.1]
   def change
     create_table :oders do |t|
+      t.integer :customer_id, null:false
       t.string :post_code, null:false
       t.string :address, null:false
       t.string :name, null:false
