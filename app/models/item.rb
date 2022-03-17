356ac12ef9d57c40e49dce4d0dcff676f18da054
cocoared <dmm23program@gmail.com>
2022-03-17 01:30:50 +0000

    Create Association

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  has_many :oder_detail
+  # belongs_to :genre
+  has_many :cart_items, dependemt: :destroy
+
 end
--- app/models/oder.rb
+++ app/models/oder.rb
@@ -1,2 +1,5 @@
 class Oder < ApplicationRecord
+  belongs_to :customer
+  has_many :oder_details
+
 end
