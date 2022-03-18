a5af054c5e6226d8aece4db59246f31231bfef5b
EC2 Default User <migayasu@gmail.com>
2022-03-18 06:32:51 +0000

    [Add]item一覧ページ 作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,5 +5,13 @@ class Public::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+  end
+
+
+  private
+
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
   end
 end
