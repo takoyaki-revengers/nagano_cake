9e8a4c9529ce5dc56810c96c5f474d26c2e75034
EC2 Default User <cnishino19820915@icloud.com>
2022-03-19 00:26:12 +0000

    [ADD] 配達先関連

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,41 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+  
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
+  end
+  
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      #flash[:notice] = "会員情報を更新しました。"
+      redirect_to :show
+    else
+      render :edit
+    end
   end
 
-  def unsubscribe
+  def unsubscribe #退会確認画面の表示
+    #@customer = current_customer
   end
+  
+  def withdraw #退会フラグを切り替える
+    @customer = current_customer
+    @user.update(is_deleted: true) #is_deletedカラムの退会フラグを退会（true）に更新する
+    reset_session #ログアウトさせる
+    #flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
+    redirect_to root_path
+  end
+  
+  
+  private
+  
+  def customer_params
+    params.require.(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :address, :telephone, :email)
+  end
+  
 end
