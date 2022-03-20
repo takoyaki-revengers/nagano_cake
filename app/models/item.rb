24de70275f1d95b825707c9f6c09fcb00e474485
EC2 Default User <migayasu@gmail.com>
2022-03-20 08:30:07 +0000

    [Add]商品一覧ページ

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,9 +14,13 @@ class Item < ApplicationRecord
   (self.price * 1.10).round
  end
 
-def get_image(width, height)
-  image.variant(resize_to_limit: [width, height]).processed
-end
+  def get_image(width, height)
+    unless image.attached?
+        file_path = Rails.root.join('app/assets/images/greater-rhea-3668046_1280.jpeg')
+        image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 
 
 
