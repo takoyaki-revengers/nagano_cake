7c966008c205c89fd6b4aca0ab102dc8bc77c055
EC2 Default User <migayasu@gmail.com>
2022-03-20 05:07:30 +0000

    [Fix]管理者seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create(
-  email:"a@a",
-  encrypted_password:"000000"
-  )
+Admin.create!(
+  email: "a@a",
+  password: "000000",
+)
