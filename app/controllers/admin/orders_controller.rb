d7a3d5f5912781573c6b03565c77220eb1b3cf2f
cocoared <dmm23program@gmail.com>
2022-03-20 07:19:02 +0000

    [Create]admin-order-controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,22 @@
 class Admin::OrdersController < ApplicationController
+
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @sum = 0
   end
 
   def update
+    order = Order.find(params[:id])
+    order_details = order.order_details
+    order.update(order_params)
+    order_details.update_all(making_status: "製作待ち") if order.status == "入金確認"
+    redirect_to request.referer
   end
+
+  private
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
