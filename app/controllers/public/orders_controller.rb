19a77da077d1d869d7d8b83c6430ec2b4d8162ba
EC2 Default User <cnishino19820915@icloud.com>
2022-03-20 09:21:34 +0000

    [ADD]注文詳細

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,6 +4,10 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
   end
+  
+  def confirm
+    
+  end
 
   def create
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
+  
+  enum payment_method: { credit_card: 0, transfer: 1 } #enumで支払方法を定義（0：クレジット、1：）
 
 end
