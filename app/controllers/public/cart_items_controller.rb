1c3fc95568a0e2ca3b0ad1d6a52a79d3feac7b2f
EC2 Default User <migayasu@gmail.com>
2022-03-20 12:18:24 +0000

    [Add]カート内商品 商品追加機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,28 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
+  end
+
+  def create
+    @cart_item = CartItem.new
+    @cart_item.customer_id = current_customer.id
+    # if
+      @cart_item.save
+      redirect_to items_path
+    # else
+      # render current_customer_cart_items_path
+    # end
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to require.referer
+  end
+
+  private
+
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :amount)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,14 +6,12 @@ class Public::ItemsController < ApplicationController
   end
 
   def show
+    @genres = Genre.all
     @item = Item.find(params[:id])
-    @cart_items = CartItem.new
+    @cart_item = CartItem.new
   end
 
 
-  private
 
-  def cart_item_params
-      params.require(:cart_item).permit(:item_id, :amount)
-  end
+ 
 end
