ae87aac678223a2769735abb056151ba2d05262b
cocoared <dmm23program@gmail.com>
2022-03-20 06:06:49 +0000

    [Add]admin-customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,25 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:page]).per(10)
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path
+  end
+
+
+  private
+  def customer_params
+    params.require(:customer).oermit(:familiy_name, :first_name, :familiy_name_kana, :first_name_kana, :post_code, :address, :email, :withdraw )
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :post_code, :address, :telephone])
   end
 
-
-
 end
