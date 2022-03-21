23edf2ea3da988215d3b7bd0a4409df1db8aec4b
cocoared <dmm23program@gmail.com>
2022-03-21 06:55:55 +0000

    [Create]管理者側 注文履歴詳細画面、注文ステータス、着手状況、製作ステータス

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -29,7 +29,7 @@ class Admin::ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     @item.update(item_params)
-    redirect_to admin_item_pash(@item)
+    redirect_to admin_item_path(@item)
   end
 
   private
