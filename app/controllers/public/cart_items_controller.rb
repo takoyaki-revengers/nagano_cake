a17b0979f07fbfe6993465c1b4e08c8d3a21248e
EC2 Default User <migayasu@gmail.com>
2022-03-21 00:26:53 +0000

    [Add]カート機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -4,11 +4,11 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new
+    @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     # if
       @cart_item.save
-      redirect_to items_path
+      redirect_to cart_items_path
     # else
       # render current_customer_cart_items_path
     # end
@@ -23,6 +23,6 @@ class Public::CartItemsController < ApplicationController
   private
 
   def cart_item_params
-      params.require(:cart_item).permit(:item_id, :amount)
+      params.require(:cart_item).permit(:item_id, :amount, :customer_id)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -8,10 +8,11 @@ class Public::ItemsController < ApplicationController
   def show
     @genres = Genre.all
     @item = Item.find(params[:id])
+    @customer_id = current_customer.id
     @cart_item = CartItem.new
   end
 
 
 
- 
+
 end
