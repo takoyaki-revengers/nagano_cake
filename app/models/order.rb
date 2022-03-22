9992e50f30859a89e65bfd73d40cced0a34b99b5
EC2 Default User <ec2-user@ip-172-31-20-106.ec2.internal>
2022-03-22 10:35:55 +0000

    [fix]管理者注文一覧編集

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,20 @@ class Order < ApplicationRecord
   has_many :order_details
 
   enum payment_method: { credit_card: 0, transfer: 1 } #enumで支払方法を定義（0：クレジット、1：銀行振込）
-
+  
+  enum status: {
+    入金待ち: 0,
+    入金確認: 1,
+    製作中: 2,
+    発送準備中: 3,
+    発送済み: 4
+  }
+  
+  def status_i18n
+   I18n.t status, scope: %i[enum orders status]
+  end
 end
+  
+  
+
+
