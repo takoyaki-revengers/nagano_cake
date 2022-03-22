ccd71456d938c5b2da20a079c553ba485222c851
EC2 Default User <ec2-user@ip-172-31-20-106.ec2.internal>
2022-03-22 00:57:53 +0000

    [add]管理者ログイン・簡易ログアウト機能

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -26,6 +26,20 @@ class Admin::SessionsController < Devise::SessionsController
   # end
 
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    admin_orders_path
+  end
+
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up)
+  end
+
 
 
 end
