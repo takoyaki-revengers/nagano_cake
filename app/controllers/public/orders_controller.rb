4f881a5bb168a53a50ce7a0741c8cf2c5ca667ae
EC2 Default User <cnishino19820915@icloud.com>
2022-03-22 02:43:01 +0000

    [add]注文情報関連

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,7 +3,7 @@ class Public::OrdersController < ApplicationController
 
   def new #注文情報入力画面を表示する(支払方法、配送先の選択)
     @order = Order.new
-    @customer = Customer.find(current_customer.id)
+    # @customer = Customer.find(current_customer.id)
   end
 
   def confirm #注文情報確認画面を表示する
@@ -11,19 +11,21 @@ class Public::OrdersController < ApplicationController
     @postage = 800 #送料
     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal } #合計金額（送料以外）
     @total_price = @total + @postage #合計金額（送料込み）
-    
-    if params[:order][:address] == "0" #自分の住所の場合
+    @order = Order.new
+    @order.customer_id = current_customer.id
+
+    if params[:order][:address_option] == "0" #自分の住所の場合
       @order = Order.new
       @order.post_code = current_customer.post_code
       @order.address = current_customer.address
       @order.name = current_customer.family_name + current_customer.first_name
-    elsif params[:order][:address] == "1" #登録済住所の場合
+    elsif params[:order][:address_option] == "1" #登録済住所の場合
       @order = Order.new
       @address = Address.find(params[:order][:address_id])
       @order.post_code = @address.post_code
       @order.address = @address.address
       @order.name = @address.name
-    elsif params[:order][:address] == "2" #新しいお届け先の場合
+    elsif params[:order][:address_option] == "2" #新しいお届け先の場合
       @order = Order.new(order_params)
     else
       #flash[:notice] = "配送先を選択してください"
@@ -33,35 +35,40 @@ class Public::OrdersController < ApplicationController
   end
 
   def create #注文情報を確定する
-    @order = Order.new(order_params)
+
+    @cart_items = current_customer.cart_items.all #会員のカート内商品全て
+    @postage = 800 #送料
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal } #合計金額（送料以外）
+    @total_price = @total + @postage #合計金額（送料込み)
     @order.customer_id = current_customer.id
 
     if @order.save
       #flash[:notice] = "注文を受け付けました"
-      
-      if params[:order][:address] == "2" #新しいお届け先を選択した場合、その住所を登録する
+
+      @cart_items.destroy_all
+      redirect_to orders_thanks_path
+    else
+      @order = Order.new(order_params)
+      render :new
+    end
+
+    if params[:order][:address] == "2" #新しいお届け先を選択した場合、その住所を登録する
         @address = Address.new
         @address.customer_id = current_customer.id
         @address.name = @order.name
         @address.post_code = @order.post_code
         @address.address = @order.address
         @address.save
-      end
-      
-        @cart_items = current_customer.cart_items #会員の注文詳細を登録する
-        @cart_items.each do |cart_item|
-        @orded_item = Order_Detail.new
-        @orded_item.item_id = cart_item.item_id
-        @orded_item.order_id = @order.id
-        @orded_item.total_payment = cart_item.item.total_payment
-        @orded_item.amount = cart_item.amount
-        @orded_item.save
-      end
-      @cart_items.destroy_all
-      redirect_to orders_thanks_path
-    else
-      @order = Order.new(order_params)
-      render :new
+    end
+
+    @cart_items = current_customer.cart_items #会員の注文詳細を登録する
+    @cart_items.each do |cart_item|
+      @orded_item = Order_Detail.new
+      @orded_item.item_id = cart_item.item_id
+      @orded_item.order_id = @order.id
+      @orded_item.total_payment = cart_item.item.total_payment
+      @orded_item.amount = cart_item.amount
+      @orded_item.save
     end
   end
 
