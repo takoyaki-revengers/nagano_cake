688e08e6230d3d4f2e029ae776ad27cda0763cbd
cnishino <96111395+cnishino@users.noreply.github.com>
2022-03-23 09:51:58 +0900

    Revert "Admin header"

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,26 +7,9 @@ class Admin::OrdersController < ApplicationController
 
 
   def show
-    @order = Order.find(params[:id])
-    @order_details = @order.order_details
-    @sum = 0
   end
 
   def update
-    @order = Order.find(params[:id])
-    @order_details = @order.order_details
-    @order.update(order_params)
-   if @order.status == "入金確認"
-     @order_details.update_all(making_status: 1)
-    redirect_to admin_order_path(@order)
-   else
-     redirect_to request.referer
-   end
-  end
-
-  private
-  def order_params
-    params.require(:order).permit(:status)
   end
 
 
