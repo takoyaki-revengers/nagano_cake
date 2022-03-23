564bf8035307adb18e8451355707b50bc73d9061
EC2 Default User <cnishino19820915@icloud.com>
2022-03-23 11:39:15 +0000

    [add]配送先フラッシュメッセージ

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :customer
+
+  validates :name, presence:true
+  validates :post_code, presence:true
+  validates :address, presence:true
+
+  def full_address #注文情報入力画面のお届け先表示用
+    '〒' + post_code + ' ' + address + ' ' + name
+  end
+
 end
