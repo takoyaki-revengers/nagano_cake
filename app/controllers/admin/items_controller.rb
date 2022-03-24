aa4a68c195ab9ab7f349404d92aa113eaaadc1d0
EC2 Default User <migayasu@gmail.com>
2022-03-24 05:05:38 +0000

    [Fix]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
     @items = Item.page(params[:page])
   end
@@ -10,12 +12,12 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-    #flash[:notice] = "新規商品の登録が完了しました。"
-    redirect_to admin_item_path(@item)
+      flash[:notice] = "新規商品の登録が完了しました。"
+      redirect_to admin_item_path(@item)
     else
-      render :new
+      flash.now[:alert] = "商品の情報を入力してください。"
+      render "new"
     end
-
   end
 
   def show
@@ -28,9 +30,13 @@ class Admin::ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
-    #flash[:notice] = "商品の更新が完了しました。"
-    redirect_to admin_item_path(@item)
+    if @item.update(item_params)
+      flash[:notice] = "商品の更新が完了しました。"
+      redirect_to admin_item_path(@item)
+    else
+      flash.now[:alert] = "商品の情報を入力してください。"
+      render "new"
+    end
   end
 
   private
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,31 +10,38 @@ class Public::OrdersController < ApplicationController
     @shipping_cost = 800 #送料
     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal } #合計金額（送料以外）
     @total_price = @total + @shipping_cost #合計金額（送料込み）
-    @order = Order.new
+
+    @order = Order.new(order_params)
     @order.customer_id = current_customer.id
 
     if params[:order][:address_option] == "0" #自分の住所の場合
-      @order = Order.new(order_params)
       @order.post_code = current_customer.post_code
       @order.address = current_customer.address
       @order.name = current_customer.family_name + current_customer.first_name
     elsif params[:order][:address_option] == "1" #登録済住所の場合
-      @order = Order.new(order_params)
       @address = Address.find(params[:order][:address_id])
       @order.post_code = @address.post_code
       @order.address = @address.address
       @order.name = @address.name
-    elsif params[:order][:address_option] == "2" #新しいお届け先の場合
-      @order = Order.new(order_params)
+    elsif params[:order][:address_option] == "2" #新しいお届け先の場合はアドレステーブルに保存まで
+      @address = Address.new
+      @address.customer_id = current_customer.id
+      @address.post_code = @order.post_code
+      @address.address = @order.address
+      @address.name = @order.name
+      if @address.save
+      else
+        flash.now[:alert] = "新しいお届け先を入力してください。"
+        @order = Order.new(order_params)
+        render :new
+      end
     else
-      #flash[:notice] = "配送先を選択してください"
-      @order = Order.new(order_params)
+      flash.now[:alert] = "配送先を選択してください。"
       render :new
     end
   end
 
   def create #注文情報を確定する
-
     @cart_items = current_customer.cart_items.all #会員のカート内商品全て
     @shipping_cost = 800 #送料
     @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal } #合計金額（送料以外）
@@ -44,34 +51,21 @@ class Public::OrdersController < ApplicationController
     @customer = Customer.find(current_customer.id)
 
     if @order.save
-      flash[:notice] = "注文を受け付けました"
       redirect_to orders_thanks_path
-
     else
-      @order = Order.new(order_params)
       render :new
     end
 
-    if params[:order][:address_option] == "2" #新しいお届け先を選択した場合、その住所を登録する
-        @address = Address.new
-        @address.customer_id = current_customer.id
-        @address.name = @order.name
-        @address.post_code = @order.post_code
-        @address.address = @order.address
-        @address.save
-    end
-
     @cart_items = current_customer.cart_items #会員の注文詳細を登録する
-    @cart_items.each do |cart_item| #購入したカート内商品全て
+    @cart_items.each do |cart_item|
       @order_detail = OrderDetail.new
-      @order_detail.item_id = cart_item.item_id #
+      @order_detail.item_id = cart_item.item_id
       @order_detail.order_id = @order.id
       @order_detail.price = cart_item.item.price
       @order_detail.amount = cart_item.amount
-      #logger.debug "************** @order_detail: #{@order_detail.attributes.inspect}"
       @order_detail.save
     end
-      @cart_items.destroy_all
+      @cart_items.destroy_all #カート内商品を削除
   end
 
   def thanks #注文完了画面を表示する
@@ -87,6 +81,8 @@ class Public::OrdersController < ApplicationController
 
   private
     def order_params
-        params.require(:order).permit(:customer_id, :total_payment, :payment_method, :post_code, :address, :name, :shipping_cost)
+      params.require(:order).permit(:customer_id, :total_payment, :payment_method, :post_code, :address, :name, :shipping_cost)
     end
+
 end
+
