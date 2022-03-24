58e69632b7d4a7477a2726a537cc0d8f967eccf7
EC2 Default User <migayasu@gmail.com>
2022-03-24 07:05:38 +0000

    [Fix]image-resize

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,14 +19,4 @@ class Item < ApplicationRecord
   (self.price * 1.10).round
  end
 
-  def get_image(width, height)
-    unless image.attached?
-        file_path = Rails.root.join('app/assets/images/greater-rhea-3668046_1280.jpeg')
-        image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    image.variant(resize_to_limit: [width, height]).processed
-  end
-
-
-
 end
