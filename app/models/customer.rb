d3badb66a90095b0ad2a0eac5481c203a2c05f40
EC2 Default User <cnishino19820915@icloud.com>
2022-03-26 01:56:24 +0000

    [fix]マイページ修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,15 @@ class Customer < ApplicationRecord
   has_many :addresses
   has_many :cart_items
 
+  validates :family_name, presence:true
+  validates :first_name, presence:true
+  validates :family_name_kana, presence:true
+  validates :first_name_kana, presence:true
+  validates :email, presence:true
+  validates :post_code, presence:true
+  validates :address, presence:true
+  validates :telephone, presence:true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
